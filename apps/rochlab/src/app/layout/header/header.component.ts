7ac8209e08bfa1d846423038344d1fdb69828b10
Hemant <hemant@Hemants-MacBook-Air.local>
2024-11-03 14:20:23 +0530

    RWD menu

--- apps/rochlab/src/app/layout/header/header.component.ts
+++ apps/rochlab/src/app/layout/header/header.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  inject,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -9,4 +15,33 @@ import { RouterModule } from '@angular/router';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  private renderer = inject(Renderer2);
+
+  @ViewChild('navmenu') navmenu!: ElementRef;
+
+  toggleMenu() {
+    if (document.body.classList.contains('mobile-nav-active')) {
+      this.closeMenu();
+    } else {
+      this.openMenu();
+    }
+  }
+
+  openMenu() {
+    this.renderer.addClass(document.body, 'mobile-nav-active');
+    // Listen for clicks outside to close the menu
+    this.renderer.listen('window', 'click', this.onClickOutside.bind(this));
+  }
+
+  closeMenu() {
+    this.renderer.removeClass(document.body, 'mobile-nav-active');
+  }
+
+  onClickOutside(event: Event) {
+    // Check if the click was outside the menu and not on a navigation link
+    if (!this.navmenu.nativeElement.contains(event.target)) {
+      this.closeMenu();
+    }
+  }
+}
