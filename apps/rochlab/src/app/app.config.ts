3e067aef6fd4056dab7917cf0d301280734b3e9a
Hemant <hemant@Hemants-MacBook-Air.local>
2024-11-03 17:52:44 +0530

    add Autorization to each request

--- apps/rochlab/src/app/app.config.ts
+++ apps/rochlab/src/app/app.config.ts
@@ -2,14 +2,17 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { environment } from '../environments/environment';
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 import { globalProviders } from '@lib/interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     globalProviders,
-    provideHttpClient(withInterceptors([])),
+    provideHttpClient(withInterceptorsFromDi()),
     provideRouter(appRoutes),
     { provide: 'ENV_CONFIG', useValue: environment },
   ],
--- libs/interceptors/src/index.ts
+++ libs/interceptors/src/index.ts
@@ -2,9 +2,10 @@
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { LoaderInterceptor } from './lib/loader-interceptor.service';
 import { CacheInterceptor } from './lib/cache.interceptor';
+import { AuthInterceptor } from './lib/auth.interceptor';
 
 export const globalProviders = [
-  // { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+  { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true },
   { provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true },
   // { provide: ErrorHandler, useClass: GlobalErrorHandler },
